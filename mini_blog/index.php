09fcf1e6fe1a98c2b713761604ff2cb06ff8bd24
shpunk <ignas.vaitkus7@gmail.com>
2022-10-04 13:53:10 +0300

    Changed folder structure and updated dependancies.

--- mini_blog/index.php
+++ mini_blog/index.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$conn)
     die("Connection failed: " . mysqli_connect_error());
 
-require_once('posts.php');
+require_once('./Modules/posts.php');
 
 $result = Posts::getPosts($conn);
 
